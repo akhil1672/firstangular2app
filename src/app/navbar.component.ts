54fda08bbbecfe1f02a6b85c925ca49acdfb8f78
akhil1672 <33376520+akhil1672@users.noreply.github.com>
2017-11-19 12:12:26 +0530

    Small changes. Added the alternate tag for images

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core'
 
 @Component({
     selector:'nav-bar',
-    template:` <h2> <div class="top-bar-title">Angular 2 Boot Camp</div></h2>
-                `,
+    template:` 
+    <h2> 
+    <div class="top-bar-title">
+    Angular 2 Boot Camp
+    </div>
+    </h2>
+        `,
     styleUrls:['./navbar.component.css']
 })
 
